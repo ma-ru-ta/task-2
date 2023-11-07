67d85c48ab4058cc31724533359f9702db2c4f15
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-07 15:59:17 +0000

    [task-4]finish-test

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,11 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
 
 end
