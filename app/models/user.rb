4701cd281c4b04f86551c2b07625b5779da7f6dd
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-09 11:13:01 +0000

    [task-5]finish-test

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,18 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
 
+ def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+ end
+
 
   def follow(user)
     active_relationships.create(followed_id: user.id)
