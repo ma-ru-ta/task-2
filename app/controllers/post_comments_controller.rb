34512d2dbd9d8032fef95ebf9b7efa61bcd9cb04
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-10 13:05:37 +0000

    [task-6]comments

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,13 +5,13 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    request.referer
   end
 
   def destroy
     comment = PostComment.find(params[:id])
     comment.destroy
-    redirect_to book_path(params[:book_id])
+    book_path(params[:book_id])
   end
 
   private
